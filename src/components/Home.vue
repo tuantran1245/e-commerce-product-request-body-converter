4400c21e0a0b1f70170ab5f011d226765dc5a9ab
Tuantq <tuantran1245@gmail.com>
2019-07-10 12:11:18 +0700

    add request body json files

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  import lazadaJson from '../Json/Lazada Request';
+  import shopeeJson from '../Json/Shopee Request';
+export default {
+  name: 'Home',
+  props: {
+  
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
